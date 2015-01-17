bc886f59f350c649b1ad0d270821f8534a217967
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-01-17 00:43:16 +0000

    - Removed SIGNATURE file. - Updated MANIFEST.SKIP file to ignore folder 'eumm/'. - Updated Copyright year information.

--- lib/Food/ECodes.pm
+++ lib/Food/ECodes.pm
@@ -1,6 +1,6 @@
 package Food::ECodes;
 
-$Food::ECodes::VERSION = '0.10';
+$Food::ECodes::VERSION = '0.11';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ Food::ECodes - Interface to Food Additive ECodes.
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =cut
 
@@ -109,7 +109,7 @@ L<http://search.cpan.org/dist/Food-ECodes/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (C) 2011 - 2014 Mohammad S Anwar.
+Copyright (C) 2011 - 2015 Mohammad S Anwar.
 
 This  program  is  free software; you can redistribute it and/or modify it under
 the  terms  of the the Artistic License (2.0). You may obtain a copy of the full
--- lib/Food/ECodes/Additive.pm
+++ lib/Food/ECodes/Additive.pm
@@ -1,6 +1,6 @@
 package Food::ECodes::Additive;
 
-$Food::ECodes::VERSION = '0.10';
+$Food::ECodes::VERSION = '0.11';
 
 =head1 NAME
 
@@ -8,7 +8,7 @@ Food::ECodes::Additive - Placeholder for Food Additive.
 
 =head1 VERSION
 
-Version 0.10
+Version 0.11
 
 =cut
 
@@ -103,7 +103,7 @@ L<http://search.cpan.org/dist/Food-ECodes/>
 
 =head1 LICENSE AND COPYRIGHT
 
-Copyright (C) 2011 - 2014 Mohammad S Anwar.
+Copyright (C) 2011 - 2015 Mohammad S Anwar.
 
 This  program  is  free software; you can redistribute it and/or modify it under
 the  terms  of the the Artistic License (2.0). You may obtain a copy of the full
